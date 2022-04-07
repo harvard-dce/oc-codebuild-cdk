11c2a08211c25b63aae484e951ae53dd07748f85
Jay Luker <jay_luker@harvard.edu>
2022-04-07 09:25:00 -0400

    store buildspec as a part of the codebuild project
    this allows the codebuild project to function for any existing git ref
    without having to include the `buildspec.yml` in the source

--- lib/oc-codebuild-cdk-stack.ts
+++ lib/oc-codebuild-cdk-stack.ts
@@ -16,18 +16,84 @@ export class OcCodebuildCdkStack extends cdk.Stack {
         webhook: true,
         branchOrRef: 'jluker-codebuild',
         owner: 'hudcede',
-        repo: 'matterhorn-dce-fork'
+        repo: 'matterhorn-dce-fork',
+        webhookFilters: [
+          codebuild.FilterGroup
+            .inEventOf(
+              codebuild.EventAction.PUSH,
+              codebuild.EventAction.PULL_REQUEST_CREATED,
+              codebuild.EventAction.PULL_REQUEST_UPDATED,
+              codebuild.EventAction.PULL_REQUEST_MERGED,
+            )
+        ]
       }),
       environment: {
-        computeType: codebuild.ComputeType.MEDIUM,
+        computeType: codebuild.ComputeType.LARGE,
         buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2,
       },
       cache: codebuild.Cache.bucket(buildBucket),
       artifacts: codebuild.Artifacts.s3({
         bucket: buildBucket,
         includeBuildId: false,
-
+        packageZip: false,
       }),
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml(
+        {
+          "version": 0.2,
+          "env": {
+            "shell": "bash",
+            "variables": {
+              "SKIP_TESTS": "-DskipTests -Dcheckstyle.skip=true",
+              "MAVEN_COMMAND": "clean install"
+            }
+          },
+          "phases": {
+            "install": {
+              "runtime-versions": {
+                "java": "corretto8"
+              },
+              "commands": [
+                "wget --no-verbose -O /opt/ffmpeg.tgz https://s3.amazonaws.com/mh-opsworks-shared-assets/ffmpeg-4.4.1-amazon-linux-static.tgz && /bin/tar -C /opt -xzf /opt/ffmpeg.tgz"
+              ]
+            },
+            "build": {
+              "commands": [
+                "echo Build started on `date`",
+                "export PATH=\"/opt/ffmpeg-4.4.1:${PATH}\"",
+                "export TZ=US/Eastern",
+                "TRIGGER_TYPE=$(echo $CODEBUILD_WEBHOOK_TRIGGER | cut -d'/' -f1)",
+                "if [ \"$TRIGGER_TYPE\" = \"pr\" ]; then SKIP_TESTS=\"\" ; MAVEN_COMMAND=\"test\" ; fi",
+                "export TRIGGER_BRANCH_OR_TAG=$(echo $CODEBUILD_WEBHOOK_TRIGGER | cut -d'/' -f2- | sed -e 's/\\//-/g')",
+                "if [[ $TRIGGER_BRANCH_OR_TAG =~ ^DCE-[0-9\\.\\-]+(-hotfix|-rc[0-9])?$ ]] ; then SKIP_TESTS=\"\" ; fi",
+                "echo $MAVEN_COMMAND",
+                "echo $SKIP_TESTS",
+                "mvn -Dmaven.repo.local=/opt/.m2/repository $MAVEN_COMMAND $SKIP_TESTS -Padmin,presentation,worker"
+              ]
+            },
+            "post_build": {
+              "commands": [
+                "echo Build completed on `date`",
+                "tar -C ./build/opencast-dist-admin-5-SNAPSHOT -czf ./build/admin.tgz .",
+                "tar -C ./build/opencast-dist-presentation-5-SNAPSHOT -czf ./build/presentation.tgz .",
+                "tar -C ./build/opencast-dist-worker-5-SNAPSHOT -czf ./build/worker.tgz .",
+                "export ARTIFACT_PATH=$TRIGGER_BRANCH_OR_TAG"
+              ]
+            }
+          },
+          "cache": {
+            "paths": [
+              "/opt/.m2/**/*"
+            ]
+          },
+          "artifacts": {
+            "discard-paths": true,
+            "files": [
+              "build/*.tgz"
+            ],
+            "name": "$ARTIFACT_PATH"
+          }
+        }
+      ),
       logging: {
         cloudWatch: {
           logGroup: new logs.LogGroup(this, `LogGroup`, {
