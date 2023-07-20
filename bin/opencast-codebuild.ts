eae68b641cb0b12c13ab0722f0193be0d3c423b0
Jay Luker <jay_luker@harvard.edu>
2023-07-20 10:48:33 -0400

    Move to CDK v2. Includes:
    - updates to most other packages (typescript, ts-node, aws-sdk, etc)
    - removing unneeded feature flags from `cdk.json`

--- bin/opencast-codebuild.ts
+++ bin/opencast-codebuild.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { SSMClient, GetParameterCommand, ParameterNotFound } from "@aws-sdk/client-ssm";
 import { OpencastCodebuild, OpencastCodebuildProps } from '../lib/opencast-codebuild-stack';
 
